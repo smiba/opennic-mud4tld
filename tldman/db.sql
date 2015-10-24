f9dc79e184ef2b1727bbcb1c45a76ce3b276074c
Bart Smienk <bart@coolmail.nl>
2015-10-24 22:14:37 +0200

    Fixed typo
    VARHCAR --> VARCHAR

--- tldman/db.sql
+++ tldman/db.sql
@@ -14,7 +14,7 @@ userid INT NOT NULL);
 create table users (
 userid INTEGER PRIMARY KEY,
 username VARCHAR(20) NOT NULL,
-password VARHCAR(32) NOT NULL,
+password VARCHAR(32) NOT NULL,
 name VARCHAR(50) NOT NULL,
 email VARCHAR(255) NOT NULL,
 country VARCHAR(2),
